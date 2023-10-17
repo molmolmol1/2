d2500ddad55d4dad5e2e2db040212662a2a57947
User <User@DESKTOP-MJCPUPU>
2023-10-17 14:50:11 +0800

    1

---
+++ qw/src/qw/Exercise1.java
@@ -0,0 +1,21 @@
+package qw;
+
+public class Exercise1 {
+	//Main method
+	public static void main (String[]args){
+		//Declare variables
+		double pounds;
+		double kilograms;
+		//Create a scanner object
+		Scanner input = new Scanner(System.in);
+		//Receive inputs from keyboards
+		System.out.println("enter a number in pounds");
+		pounds = input.nextdouble();
+		//compute kilograms
+		//one pounds is 0.454 kilograms
+		kilograms = pounds * 0.454;
+		//display result
+		System.out.println(pounds + "is" + kilograms + "kilograms")
+		
+	}
+}
